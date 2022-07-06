41dc2f5aeecc09813963e84d2eede2b60b1adf93
lwebb-dev <logan@lwebb.dev>
2022-07-06 10:15:43 -0400

    feat: add default values to .env build script

--- scripts/build-env-files.py
+++ scripts/build-env-files.py
@@ -1,16 +1,28 @@
-import os
+import sys, os
 
 repoRoot = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 projects = [ "src/backend/PokeCache", "src/backend/PokeQuery", "src/frontend/poke-query" ]
 envVars = [ "MAX_RESULT_SIZE", "API_BASE_URI", "REDIS_CONNECTION_STRING" ]
+defaultValues = [ 3, "http://localhost:5244", "localhost:6379" ]
+useDefaultValues = len(sys.argv) == 2 and sys.argv[1].lower() == "--use-default"
 values = []
 
-os.system("cls" if os.name == "nt" else "clear")
+def setValues():
+    if (useDefaultValues):
+        for i in range(len(envVars)):
+            values.append(f"{envVars[i]}={defaultValues[i]}\n")
+        return
+    
+    for i in range(len(envVars)):
+        value = input(f"{envVars[i]} ({defaultValues[i]}): ")
 
-for x in envVars:
-    value = input(f"{x}: ")
-    value = f"{x}={value}\n"
-    values.append(value)
+        if (value == ""):
+            value = defaultValues[i]
+
+        value = f"{envVars[i]}={value}\n"
+        values.append(value)
+
+setValues()
 
 for project in projects:
     filepath = f"{repoRoot}/{project}/.env"
