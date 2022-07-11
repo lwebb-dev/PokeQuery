4d110ea7b891dc9d73ec3e04602490b9e9ab4935
lwebb-dev <logan@lwebb.dev>
2022-07-11 11:15:26 -0400

    chore: frontend ui updates

--- src/frontend/poke-query/src/App.svelte
+++ src/frontend/poke-query/src/App.svelte
@@ -6,6 +6,37 @@
     Items = 2
   }
 
+  let typeColors = {
+    "grass": "#78C850",
+    "fire": "#F08030",
+    "water": "#6890F0",
+    "bug": "#A8B820",
+    "normal": "#A8A878",
+    "flying": "#a396e0",
+    "poison": "#A040A0",
+    "electric": "#F8D030",
+    "ground": "#E0C068",
+    "fairy": "#EE99AC",
+    "fighting": "#C03028",
+    "psychic": "#F85888",
+    "rock": "#B8A038",
+    "ghost": "#705898",
+    "ice": "#98D8D8",
+    "dragon": "#7038F8",
+    "steel": "#919191",
+    "dark": "#2E291B",
+  }
+
+  const getFontColorByPkmnType = (type: string): string => {
+
+    const blackTypes: string[] = [ "electric", "fairy", "ice" ];
+
+    if (blackTypes.includes(type))
+      return "black";
+
+    return "white";
+  }
+
   let results: any[] = [];
   let query: string;
   let includePokemon: boolean = true;
@@ -94,26 +125,26 @@
     {#each results as result}
 
       {#if result.resourceType == ResourceTypes.Pokemon}
-        <div class="card mw-20 m-2 card-pkmn">
+        <div class="card mw-20 h-50 m-2 card-pkmn">
           <img class="card-img-top h-35 pt-2 ps-5 pe-5" src="{result.json.Sprites.FrontDefault}" alt="{result.name}"/>
           <div class="card-body">
-            <h4 class="card-title text-capitalize text-center">{result.json.Name}</h4>
-            <div class="d-flex justify-content-center">
+            <h4 class="card-title text-capitalize text-center">{result.json.Name.replaceAll('-', ' ')}</h4>
+            <div class="d-flex justify-content-center" style="height:3em;">
               {#each result.json.Types as pkmnType}
-                <div class="card-text m-2 text-capitalize">
+                <div class="card-text m-2 pt-1 ps-3 pe-3 text-capitalize" style="background-color:{typeColors[pkmnType.Type.Name]};border-radius:15px;color:{getFontColorByPkmnType(pkmnType.Type.Name)};font-size:small">
                   <p>{pkmnType.Type.Name}</p>
                 </div>
               {/each}
             </div>
-            <p class="card-text">Here's some more filler text to give the test cards some more body untill we parse the pokemon json data and add more of it to the display.</p>  
+            <!-- <p class="card-text">Here's some more filler text to give the test cards some more body untill we parse the pokemon json data and add more of it to the display.</p>   -->
           </div>
         </div>
 
       {:else if result.resourceType == ResourceTypes.Items}
-      <div class="card mw-20 m-2 card-item">
+      <div class="card mw-20 h-50 m-2 card-item">
         <img  class="card-img-top h-35 pt-2 ps-5 pe-5" src="{result.json.Sprites.Default}" alt="{result.name}"/>
         <div class="card-body">
-          <h4 class="card-title text-capitalize text-center">{result.json.Name}</h4>
+          <h4 class="card-title text-capitalize text-center">{result.json.Name.replaceAll('-', ' ')}</h4>
           {#if typeof(result.json.EffectEntries[0]) !== "undefined"}
             <p class="card-text text-wrap">{result.json.EffectEntries[0].ShortEffect}</p>
           {/if}  
@@ -121,9 +152,9 @@
       </div>
 
       {:else}
-        <div class="card mw-20 m-2 card-move">
+        <div class="card mw-20 h-50 m-2 card-move">
           <div class="card-body">
-            <h4 class="card-title text-capitalize text-center">{result.json.Name}</h4>
+            <h4 class="card-title text-capitalize text-center">{result.json.Name.replaceAll('-', ' ')}</h4>
             {#if typeof(result.json.EffectEntries[0]) !== "undefined"}
               <p class="card-text text-wrap">{interpolateEffectChance(result.json.EffectEntries[0].ShortEffect, result.json.EffectChance)}</p>
             {/if}  
@@ -145,18 +176,21 @@
   }
 
   .card-pkmn {
-    background-color: #FC8686;
-    width: 18rem;
+    border-color: #FC8686;
+    border-width: 0.35em;
+    width: 15rem;
   }
 
   .card-item {
-    background-color: #ECF296;
-    width: 18rem;
+    border-color: #ECF296;
+    border-width: 0.35em;
+    width: 15rem;
   }
 
   .card-move {
-    background-color: #BEE4FA;
-    width: 18rem;
+    border-color: #BEE4FA;
+    border-width: 0.35em;
+    width: 15rem;
   }
 
 
