c2510d5c147ba24fb57db0609dd081a81ab6b714
lwebb-dev <logan@lwebb.dev>
2022-07-14 15:26:51 -0400

    refactor: use string.Join for SortIndex

--- src/backend/PokeLib/Models/CachedResource.cs
+++ src/backend/PokeLib/Models/CachedResource.cs
@@ -6,6 +6,10 @@
         public ResourceTypes ResourceType { get; set; }
         public string Url { get; set; }
         public string Json { get; set; }
-        public string SortIndex => $"{(int)this.ResourceType}_{this.Name}";
+        public string SortIndex => string.Join('_', new[] 
+        { 
+            ((int)this.ResourceType).ToString(), 
+            this.Name 
+        });
     }
 }
