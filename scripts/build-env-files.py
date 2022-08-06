5fb3645671dbcb0574741a5d2c385ba47d9aa110
lwebb-dev <logan@lwebb.dev>
2022-08-06 16:34:08 -0400

    chore: rename legacy backend dir

--- scripts/build-env-files.py
+++ scripts/build-env-files.py
@@ -1,7 +1,7 @@
 import sys, os
 
 repoRoot = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-projects = [ "src/backend/PokeCache", "src/backend/PokeQuery", "src/frontend/poke-query" ]
+projects = [ "src/backend-legacy/PokeCache", "src/backend-legacy/PokeQuery", "src/frontend/poke-query" ]
 envVars = [ "MAX_RESULT_SIZE", "API_BASE_URI", "REDIS_CONNECTION_STRING" ]
 defaultValues = [ 12, "http://localhost:5244", "localhost:6379" ]
 useDefaultValues = len(sys.argv) == 2 and sys.argv[1].lower() == "--use-default"
--- scripts/run-poke-cache.py
+++ scripts/run-poke-cache.py
@@ -1,5 +1,5 @@
 import os, subprocess
 
 baseDir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-pokeCacheDir = f"{baseDir}\\src\\backend\\PokeCache"
+pokeCacheDir = f"{baseDir}\\src\\backend-legacy\\PokeCache"
 subprocess.check_call(f"cd {pokeCacheDir} && dotnet run --project .\PokeCache.csproj", shell=True)
