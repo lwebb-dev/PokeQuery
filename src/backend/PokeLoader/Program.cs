2f877c8dbdd7011d8171f066369fd37d079d2fc6
lwebb-dev <logan@lwebb.dev>
2022-08-07 11:03:41 -0400

    feat: new backend sln w/ docker compose support

---
+++ src/backend/PokeLoader/Program.cs
@@ -0,0 +1,10 @@
+using PokeLoader;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
