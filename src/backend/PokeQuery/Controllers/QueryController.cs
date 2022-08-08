f6ec8104805858b3d0039b91327e30b57e2c34d2
lwebb-dev <logan@lwebb.dev>
2022-08-08 08:05:15 -0400

    feat: new barebones containerized pokequery api project

---
+++ src/backend/PokeQuery/Controllers/QueryController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokeApiNet;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PokeQuery.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QueryController : ControllerBase
+    {
+        //private readonly IRedisQueryService queryService;
+
+        public QueryController()
+        {
+            //this.queryService = queryService;
+        }
+
+        [HttpGet("/types")]
+        public async Task<ActionResult<IEnumerable<PokeApiNet.Type>>> GetTypesAsync()
+        {
+            throw new NotImplementedException();
+            //return Ok(await this.queryService.GetNamedResourceAsync<Type>(NamedResourceTypes.Types));
+        }
+
+        [HttpGet("/version-groups")]
+        public async Task<ActionResult<IEnumerable<VersionGroup>>> GetVersionGroupsAsync()
+        {
+            throw new NotImplementedException();
+            //return Ok(await this.queryService.GetNamedResourceAsync<VersionGroup>(NamedResourceTypes.VersionGroups));
+        }
+
+        [HttpGet("/generations")]
+        public async Task<ActionResult<IEnumerable<Generation>>> GetGenerationsAsync()
+        {
+            throw new NotImplementedException();
+            //return Ok(await this.queryService.GetNamedResourceAsync<Generation>(NamedResourceTypes.Generations));
+        }
+
+        [HttpGet("/machines")]
+        public async Task<ActionResult<IEnumerable<Machine>>> GetMachinesAsync()
+        {
+            throw new NotImplementedException();
+            //return Ok(await this.queryService.GetApiResourceAsync<Machine>(ResourceTypes.Machines));
+        }
+    }
+}
---
+++ src/backend/PokeQuery/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PokeQuery
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
