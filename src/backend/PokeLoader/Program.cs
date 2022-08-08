5b5218056fb681b6406cad30b7d0b813a5202171
lwebb-dev <logan@lwebb.dev>
2022-08-08 04:30:56 -0400

    feat: PokeLoader - Load PokeAPI json data into Redis-Stack

--- src/backend/PokeLoader/Program.cs
+++ src/backend/PokeLoader/Program.cs
@@ -1,10 +1,72 @@
-using PokeLoader;
+﻿using NRediSearch;
+using NReJSON;
+using StackExchange.Redis;
+using System;
+using System.IO;
+using System.Linq;
+using static NRediSearch.Client;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+public class Program
+{
+    public static void Main(string[] args)
     {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+        string REDIS_CONNECTION = Environment.GetEnvironmentVariable("REDIS_CONNECTION");
+        string CACHE_DIRECTORY = Environment.GetEnvironmentVariable("CACHE_DIRECTORY");
 
-await host.RunAsync();
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(REDIS_CONNECTION);
+        IDatabase db = redis.GetDatabase();
+
+        foreach (string resourceDir in Directory.GetDirectories(CACHE_DIRECTORY))
+        {
+            Uri resourceUri  = new Uri(resourceDir);
+            string index = resourceUri.Segments.Last();
+            DropCreateIndex(db, index);
+
+            foreach (string itemDir in Directory.GetDirectories(resourceDir))
+            {
+                Uri itemUri = new Uri(itemDir);
+                string id = itemUri.Segments.Last();
+                string[] files = Directory.GetFiles(itemDir);
+                string json = File.ReadAllText(files.SingleOrDefault(x => x.Contains("index.json")));
+                RedisKey key = new RedisKey($"{index}:{id}");
+                OperationResult result = db.JsonSet(key, json);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine($"{key} failed to write.");
+                }
+            }
+        }
+
+        Console.WriteLine("Done!");
+    }
+
+    public static void DropCreateIndex(IDatabase db, string index)
+    {
+        string namedIndex = $"idx:{index}";
+
+        try
+        {
+            db.Execute("FT.DROPINDEX", namedIndex);
+
+        }
+        catch
+        {
+
+        }
+
+        var schema = new Schema()
+            .AddTextField("$.name");
+
+        var options = new ConfiguredIndexOptions(
+            new IndexDefinition(
+                type: IndexDefinition.IndexType.Json, 
+                prefixes: new[] { $"{index}:" }
+            )
+        );
+
+        Client searchNameClient = new Client(namedIndex, db);
+        bool result = searchNameClient.CreateIndex(schema, options);
+        Console.WriteLine($"{namedIndex}: {result}");
+    }
+}
