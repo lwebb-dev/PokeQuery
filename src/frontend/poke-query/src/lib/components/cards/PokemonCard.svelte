9f457c507bd1e67c90a4b7fc3c1b3ca1e38e7504
lwebb-dev <logan@lwebb.dev>
2022-08-22 23:39:00 -0400

    feat: add shiny sprite button

--- src/frontend/poke-query/src/lib/components/cards/PokemonCard.svelte
+++ src/frontend/poke-query/src/lib/components/cards/PokemonCard.svelte
@@ -4,12 +4,21 @@
   import TypeModal from "../modals/TypeModal.svelte";
 
   export let data;
+
+  let shinyBtnSelected = false;
+
+  const stripGarbageSpriteText = (text) => {
+    return text?.replace(
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites", 
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites");
+  }
 </script>
 
 <div class="card mw-20 m-2 card-pkmn">
+  <button class="shiny-btn" class:shiny-selected="{shinyBtnSelected}" on:click="{() => shinyBtnSelected = !shinyBtnSelected}">✨</button>
   <img
     class="card-img-top h-45 pt-2 ps-5 pe-5"
-    src={data.sprites.other.home.front_default?.replace("https://raw.githubusercontent.com/PokeAPI/sprites/master/https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites")}
+    src={stripGarbageSpriteText(shinyBtnSelected ? data.sprites.other.home.front_shiny : data.sprites.other.home.front_default)}
     alt={data.name}
   />
   <div class="card-body">
@@ -38,5 +47,29 @@
     border-width: 0.35em;
     width: 15rem;
     height: 21rem;
+  }
+
+  .shiny-btn {
+    position: absolute;
+    text-align: center;
+    height: 40px;
+    width: 40px;
+    margin-top: 0.4rem;
+    margin-left: 11rem;
+    border-radius: 25%;
+    border-color: rgba(0, 0, 0, 0.0);
+    background-color: rgba(0, 0, 0, 0.0);
+    filter: grayscale(100%);
+    cursor: pointer;
+    z-index: 5;
+  }
+
+  .shiny-btn:hover {
+    background-color: #efef99;
+  }
+
+  .shiny-selected {
+    filter: grayscale(100%) sepia(100%) saturate(500%);
+
   }
 </style>
