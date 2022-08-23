1f8850a32876143d187970e14b971cdd34c1081c
lwebb-dev <logan@lwebb.dev>
2022-08-23 17:31:19 -0400

    refactor: clean up MovesModal

--- src/frontend/poke-query/src/lib/components/modals/MovesModal.svelte
+++ src/frontend/poke-query/src/lib/components/modals/MovesModal.svelte
@@ -17,6 +17,10 @@
     sessionVersionGroups[i] = JSON.parse(sessionVersionGroups[i]);
   }
 
+  for (let i = 0; i < sessionMachineData.length; i++) {
+    sessionMachineData[i] = JSON.parse(sessionMachineData[i]);
+  }
+
   let versionGroups = [];
   let machineMoveDict = [];
   let pkmnSpecies, basePkmnSpecies;
@@ -33,94 +37,62 @@
     return getBasePkmnSpecies(evolvesFromSpecies);
   }
 
-  const getBasePkmnData = async (id) => {
-    if (basePkmnSpecies.id === pkmnSpecies.id) {
-      return await data;
-    }
-
-    return await fetchJson(baseUri, "pokemon", basePkmnSpecies.id);
+  const getBasePkmnData = async () => {
+    return (basePkmnSpecies.id === pkmnSpecies.id) ? data : await fetchJson(baseUri, "pokemon", basePkmnSpecies.id);
   }
 
   const getMachine = (move) => {
-    const machine = sessionMachineData.find((x) => { 
-      const moveId = getIdFromUrl(move.move.url);
-      const machineJsonObject = JSON.parse(x);
-      const machineMoveUrlId = getIdFromUrl(machineJsonObject.move.url);
-      return machineMoveUrlId === moveId && machineJsonObject.version_group.name === move.version_group_details[0].version_group.name;
-     });
-
-    return JSON.parse(machine).item.name;
+    const machine = sessionMachineData
+    .find((x) => getIdFromUrl(x.move.url) === getIdFromUrl(move.move.url) && x.version_group.name === move.version_group_details[0].version_group.name);
+    return machine.item.name;
   };
+
+  const filterBySelectedVersion = (data) => {
+    return data.filter(
+      (x) =>
+        (x.version_group_details = x.version_group_details.filter(
+          (vgd) => vgd.version_group.name === selectedVersion
+        ))
+    );
+  }
+
+  const filterByLearnMethod = (data, learnMethod) => {
+    data.forEach(
+      (x) =>
+        (x.version_group_details = x.version_group_details.filter(
+          (vgd) => vgd.move_learn_method.name === learnMethod
+        ))
+    );
+
+    return data;
+  }
+
+  const filterByHasVersionGroupDetails = (data) => {
+    return data.filter((x) => x.version_group_details.length > 0);
+  }
 
   const loadMoveDataByVersion = () => {
 
-    let moveDataClone = structuredClone(moveData);
-    let baseMoveDataClone = structuredClone(baseMoveData);
-
-    let versionMoveData = moveDataClone.filter(
-      (x) =>
-        (x.version_group_details = x.version_group_details.filter(
-          (vgd) => vgd.version_group.name === selectedVersion
-        ))
-    );
-
-    let baseVersionMoveData = baseMoveDataClone.filter(
-      (x) =>
-        (x.version_group_details = x.version_group_details.filter(
-          (vgd) => vgd.version_group.name === selectedVersion
-        ))
-    );
-
-    lvlUpMoveData = structuredClone(versionMoveData);
-    lvlUpMoveData.forEach(
-      (x) =>
-        (x.version_group_details = x.version_group_details.filter(
-          (vgd) => vgd.move_learn_method.name === "level-up"
-        ))
-    );
-    lvlUpMoveData = lvlUpMoveData
-      .filter((x) => x.version_group_details.length > 0)
-      .sort(
-        (a, b) =>
-          a.version_group_details[0].level_learned_at -
-          b.version_group_details[0].level_learned_at
-      );
-
-    machineMoveData = structuredClone(versionMoveData);
     machineMoveDict = [];
-    machineMoveData.forEach(
-      (x) =>
-        (x.version_group_details = x.version_group_details.filter(
-          (vgd) => vgd.move_learn_method.name === "machine"
-        ))
-    );
-    machineMoveData = machineMoveData.filter(
-      (x) => x.version_group_details.length > 0
-    );
+    let versionMoveData = filterBySelectedVersion(structuredClone(moveData));
+    let baseVersionMoveData = filterBySelectedVersion(structuredClone(baseMoveData));
+
+    lvlUpMoveData = filterByLearnMethod(structuredClone(versionMoveData), "level-up");
+    machineMoveData = filterByLearnMethod(structuredClone(versionMoveData), "machine");
+    eggMoveData = filterByLearnMethod(structuredClone(baseVersionMoveData), "egg");
+    tutorMoveData = filterByLearnMethod(structuredClone(versionMoveData), "tutor");
+
+    lvlUpMoveData = filterByHasVersionGroupDetails(lvlUpMoveData);
+    machineMoveData = filterByHasVersionGroupDetails(machineMoveData);
+    eggMoveData = filterByHasVersionGroupDetails(eggMoveData);
+    tutorMoveData = filterByHasVersionGroupDetails(tutorMoveData);
+
     machineMoveData.forEach((x) => {
       machineMoveDict.push({machine: getMachine(x), data: x});
     });
-    machineMoveDict.sort((a, b) => a.machine.localeCompare(b.machine) );
 
-    eggMoveData = structuredClone(baseVersionMoveData);
-    eggMoveData.forEach(
-      (x) =>
-        (x.version_group_details = x.version_group_details.filter(
-          (vgd) => vgd.move_learn_method.name === "egg"
-        ))
-    );
-    eggMoveData = eggMoveData.filter((x) => x.version_group_details.length > 0);
-
-    tutorMoveData = structuredClone(versionMoveData);
-    tutorMoveData.forEach(
-      (x) =>
-        (x.version_group_details = x.version_group_details.filter(
-          (vgd) => vgd.move_learn_method.name === "tutor"
-        ))
-    );
-    tutorMoveData = tutorMoveData.filter(
-      (x) => x.version_group_details.length > 0
-    );
+    lvlUpMoveData.sort((a, b) => a.version_group_details[0].level_learned_at - b.version_group_details[0].level_learned_at);
+    machineMoveDict.sort((a, b) => a.machine.localeCompare(b.machine));
   };
 
   onMount(async () => {
@@ -148,75 +120,41 @@
 </script>
 
 <!-- Button trigger modal -->
-<button
-  type="button"
-  class="btn btn-primary mx-1"
-  data-bs-toggle="modal"
-  data-bs-target="#{modalName}"
->
+<button type="button" class="btn btn-primary mx-1" data-bs-toggle="modal" data-bs-target="#{modalName}">
   Moves
 </button>
 
 <!-- Modal -->
-<div
-  class="modal fade"
-  id={modalName}
-  tabindex="-1"
-  aria-labelledby="{modalName}-Label"
-  aria-hidden="true"
->
-  <div
-    class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable"
-  >
+<div class="modal fade" id={modalName} tabindex="-1" aria-labelledby="{modalName}-Label" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title text-capitalize" id="{modalName}-Label">
           Moves: {data.name}
         </h5>
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="modal"
-          aria-label="Close"
-        />
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
       </div>
       <div class="modal-body">
         <div class="container modal-container px-5">
           <label for="version-select" class="my-0" style="display: block;">Version(s):</label>
-          <select
-            id="version-select"
-            class="form-select-lg my-3 mt-1 text-capitalize"
-            aria-label="version group select"
-            bind:value={selectedVersion}
-            on:change={() => loadMoveDataByVersion()}
-          >
+          <select id="version-select" class="form-select-lg my-3 mt-1 text-capitalize" aria-label="version group select" 
+          bind:value={selectedVersion} 
+          on:change={() => loadMoveDataByVersion()}>
             {#each versionGroups as version}
-              <option class="text-capitalize" value={version}>{version.replaceAll('-', ' ')}</option>
+              <option class="text-capitalize" value={version}>
+                {version.replaceAll('-', ' ')}
+              </option>
             {/each}
           </select>
 
           <div class="accordion" id="moveModalAccordion-{modalName}">
             <div class="accordion-item">
-              <h2
-                class="accordion-header"
-                id="panelsStayOpen-headingOne-{modalName}"
-              >
-                <button
-                  class="accordion-button"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseOne-{modalName}"
-                  aria-expanded="true"
-                  aria-controls="panelsStayOpen-collapseOne-{modalName}"
-                >
+              <h2 class="accordion-header" id="panelsStayOpen-headingOne-{modalName}">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne-{modalName}" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne-{modalName}">
                   Level Up
                 </button>
               </h2>
-              <div
-                id="panelsStayOpen-collapseOne-{modalName}"
-                class="accordion-collapse collapse show"
-                aria-labelledby="panelsStayOpen-headingOne-{modalName}"
-              >
+              <div id="panelsStayOpen-collapseOne-{modalName}" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne-{modalName}">
                 <div class="accordion-body scrolling-table">
                   <table class="table">
                     <thead>
@@ -240,27 +178,12 @@
               </div>
             </div>
             <div class="accordion-item">
-              <h2
-                class="accordion-header"
-                id="panelsStayOpen-headingTwo-{modalName}"
-              >
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  id="machineButton"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseTwo-{modalName}"
-                  aria-expanded="false"
-                  aria-controls="panelsStayOpen-collapseTwo-{modalName}"
-                >
+              <h2 class="accordion-header" id="panelsStayOpen-headingTwo-{modalName}">
+                <button class="accordion-button collapsed" type="button" id="machineButton"  data-bs-toggle="collapse"  data-bs-target="#panelsStayOpen-collapseTwo-{modalName}"  aria-expanded="false"  aria-controls="panelsStayOpen-collapseTwo-{modalName}">
                   Machine
                 </button>
               </h2>
-              <div
-                id="panelsStayOpen-collapseTwo-{modalName}"
-                class="accordion-collapse collapse"
-                aria-labelledby="panelsStayOpen-headingTwo-{modalName}"
-              >
+              <div id="panelsStayOpen-collapseTwo-{modalName}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo-{modalName}">
                 <div class="accordion-body scrolling-table">
                   <table class="table">
                     <thead>
@@ -282,26 +205,12 @@
               </div>
             </div>
             <div class="accordion-item">
-              <h2
-                class="accordion-header"
-                id="panelsStayOpen-headingThree-{modalName}"
-              >
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseThree-{modalName}"
-                  aria-expanded="false"
-                  aria-controls="panelsStayOpen-collapseThree-{modalName}"
-                >
+              <h2 class="accordion-header" id="panelsStayOpen-headingThree-{modalName}">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree-{modalName}" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree-{modalName}">
                   Egg
                 </button>
               </h2>
-              <div
-                id="panelsStayOpen-collapseThree-{modalName}"
-                class="accordion-collapse collapse"
-                aria-labelledby="panelsStayOpen-headingThree-{modalName}"
-              >
+              <div id="panelsStayOpen-collapseThree-{modalName}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree-{modalName}">
                 <div class="accordion-body scrolling-table">
                   <table class="table">
                     <thead>
@@ -321,26 +230,12 @@
               </div>
             </div>
             <div class="accordion-item">
-              <h2
-                class="accordion-header"
-                id="panelsStayOpen-headingFour-{modalName}"
-              >
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapseFour-{modalName}"
-                  aria-expanded="false"
-                  aria-controls="panelsStayOpen-collapseFour-{modalName}"
-                >
+              <h2 class="accordion-header"id="panelsStayOpen-headingFour-{modalName}">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour-{modalName}" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour-{modalName}">
                   Tutor
                 </button>
               </h2>
-              <div
-                id="panelsStayOpen-collapseFour-{modalName}"
-                class="accordion-collapse collapse"
-                aria-labelledby="panelsStayOpen-headingFour-{modalName}"
-              >
+              <div id="panelsStayOpen-collapseFour-{modalName}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour-{modalName}">
                 <div class="accordion-body scrolling-table">
                   <table class="table">
                     <thead>
@@ -362,9 +257,9 @@
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-primary" data-bs-dismiss="modal"
-            >Close</button
-          >
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+            Close
+            </button>
         </div>
       </div>
     </div>
